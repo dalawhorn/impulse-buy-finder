dfc1fa6a8f4f5a2315c238f430f5ce063ff79439
Daniel Lawhorn <dalawhorn21@gmail.com>
2020-09-08 15:40:52 -0400

    Product page styling.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 @section('body')
     <div class="container mx-auto bg-white h-screen">
         <div class="container mx-auto py-4" id="header">
-            <h1>Impulse Buy Finder</h1>
+            {{--<h1>Impulse Buy Finder</h1>--}}
         </div>
         <div class="container mx-auto">
             @yield('content')
